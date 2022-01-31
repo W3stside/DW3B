fa96bc5147f81e90c90d27350b7147c4a1f093db
David W3stside <david.sato64@gmail.com>
2022-01-30 23:57:40 -0300

    cleanup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,96 +1,16 @@
-import { AbstractConnector } from '@web3-react/abstract-connector'
-import { SupportedChainId as ChainId } from './chains'
-
-import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
-
-export * from './blockchain'
 export * from './tokens'
 export * from './chains'
 export * from './addresses'
-
-export const WETH: { [chain in ChainId]: string } = {
-  [ChainId.MAINNET]: '1',
-  [ChainId.RINKEBY]: '4',
-  [ChainId.XDAI]: '100'
-}
+export * from './wallets'
 
 // Block time here is slightly higher (~1s) than average in order to avoid ongoing proposals past the displayed time
 export const AVERAGE_BLOCK_TIME_IN_SECS = 14
 
-export interface WalletInfo {
-  connector?: AbstractConnector
-  name: string
-  iconName: string
-  description: string
-  href: string | null
-  color: string
-  primary?: true
-  mobile?: true
-  mobileOnly?: true
-}
-
-export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
-  INJECTED: {
-    connector: injected,
-    name: 'Injected',
-    iconName: 'arrow-right.svg',
-    description: 'Injected web3 provider.',
-    href: null,
-    color: '#010101',
-    primary: true
-  },
-  METAMASK: {
-    connector: injected,
-    name: 'MetaMask',
-    iconName: 'metamask.png',
-    description: 'Easy-to-use browser extension.',
-    href: null,
-    color: '#E8831D'
-  },
-  WALLET_CONNECT: {
-    connector: walletconnect,
-    name: 'WalletConnect',
-    iconName: 'walletConnectIcon.svg',
-    description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
-    href: null,
-    color: '#4196FC',
-    mobile: true
-  },
-  WALLET_LINK: {
-    connector: walletlink,
-    name: 'Coinbase Wallet',
-    iconName: 'coinbaseWalletIcon.svg',
-    description: 'Use Coinbase Wallet app on mobile device',
-    href: null,
-    color: '#315CF5'
-  },
-  COINBASE_LINK: {
-    name: 'Open in Coinbase Wallet',
-    iconName: 'coinbaseWalletIcon.svg',
-    description: 'Open in Coinbase Wallet app.',
-    href: 'https://go.cb-w.com/mtUDhEZPy1',
-    color: '#315CF5',
-    mobile: true,
-    mobileOnly: true
-  },
-  FORTMATIC: {
-    connector: fortmatic,
-    name: 'Fortmatic',
-    iconName: 'fortmaticIcon.png',
-    description: 'Login using Fortmatic hosted wallet',
-    href: null,
-    color: '#6748FF',
-    mobile: true
-  },
-  Portis: {
-    connector: portis,
-    name: 'Portis',
-    iconName: 'portisIcon.png',
-    description: 'Login using Portis hosted wallet',
-    href: null,
-    color: '#4A6C9B',
-    mobile: true
-  }
-}
-
-export const NetworkContextName = 'NETWORK'
+export const DEFAULT_DECIMALS = 18
+export const DEFAULT_PRECISION = 6
+export const DEFAULT_SMALL_LIMIT = '0.000001'
+export const AMOUNT_PRECISION = 4
+export const LONG_PRECISION = 10
+export const FULL_PRICE_PRECISION = 20
+export const FIAT_PRECISION = 2
+export const PERCENTAGE_PRECISION = 2
