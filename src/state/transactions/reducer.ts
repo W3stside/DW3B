ab30f0b194718ae8dc1bebb5746792131264572c
David W3stside <david.sato64@gmail.com>
2022-02-01 13:13:54 -0300

    update transactions reducer

--- src/state/transactions/reducer.ts
+++ src/state/transactions/reducer.ts
@@ -1,65 +1,193 @@
-import { createReducer } from '@reduxjs/toolkit'
-import {
-  addTransaction,
-  checkedTransaction,
-  clearAllTransactions,
-  finalizeTransaction,
-  SerializableTransactionReceipt
-} from './actions'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { SafeMultisigTransactionResponse } from '@gnosis.pm/safe-service-client'
 
-const now = () => new Date().getTime()
-
-export interface TransactionDetails {
-  hash: string
-  approval?: { tokenAddress: string; spender: string }
-  summary?: string
-  claim?: { recipient: string }
-  receipt?: SerializableTransactionReceipt
-  lastCheckedBlockNumber?: number
-  addedTime: number
-  confirmedTime?: number
+type WithChainId = { chainId: number }
+type WithData = { data?: any }
+type WithChainIdAndHash = WithChainId & { hash: string }
+export interface SerializableTransactionReceipt {
+  to: string
   from: string
+  contractAddress: string
+  transactionIndex: number
+  blockHash: string
+  transactionHash: string
+  blockNumber: number
+  status?: number
 }
 
-export interface TransactionState {
+export type ReplacementType = 'speedup' | 'cancel'
+
+export type AddTransactionParams = WithChainId &
+  WithData &
+  Pick<
+    EnhancedTransactionDetails,
+    'hash' | 'hashType' | 'from' | 'approval' | 'presign' | 'claim' | 'summary' | 'safeTransaction'
+  >
+
+export enum HashType {
+  ETHEREUM_TX = 'ETHEREUM_TX',
+  GNOSIS_SAFE_TX = 'GNOSIS_SAFE_TX'
+}
+
+export interface EnhancedTransactionDetails {
+  hash: string // The hash of the transaction, normally Ethereum one, but not necessarily
+  hashType: HashType // Transaction hash: could be Ethereum tx, or for multisigs could be some kind of hash identifying the order (i.e. Gnosis Safe)
+  transactionHash?: string // Transaction hash. For EOA this field is immediately available, however, other wallets go through a process of offchain signing before the transactionHash is available
+
+  // Params using for polling handling
+  addedTime: number // Used to determine the polling frequency
+  lastCheckedBlockNumber?: number
+
+  // Basic data
+  from: string
+  summary?: string
+  confirmedTime?: number
+  receipt?: SerializableTransactionReceipt // Ethereum transaction receipt
+  data?: any // any attached data type
+
+  // Operations
+  approval?: { tokenAddress: string; spender: string }
+  presign?: { orderId: string }
+  claim?: { recipient: string; cowAmountRaw?: string; indices: number[] }
+
+  // Wallet specific
+  safeTransaction?: SafeMultisigTransactionResponse // Gnosis Safe transaction info
+  replacementType?: ReplacementType // if the user cancelled or speedup the tx it will be reflected here
+}
+
+export interface EnhancedTransactionState {
   [chainId: number]: {
-    [txHash: string]: TransactionDetails
+    [txHash: string]: EnhancedTransactionDetails
   }
 }
 
-export const initialState: TransactionState = {}
+export const initialState: EnhancedTransactionState = {}
 
-export default createReducer(initialState, builder =>
-  builder
-    .addCase(addTransaction, (transactions, { payload: { chainId, from, hash, approval, summary, claim } }) => {
+const now = () => new Date().getTime()
+
+function updateBlockNumber(tx: EnhancedTransactionDetails, blockNumber: number) {
+  if (!tx.lastCheckedBlockNumber) {
+    tx.lastCheckedBlockNumber = blockNumber
+  } else {
+    tx.lastCheckedBlockNumber = Math.max(blockNumber, tx.lastCheckedBlockNumber)
+  }
+}
+
+const transactionsSlice = createSlice({
+  name: 'transactions',
+  initialState,
+  reducers: {
+    addTransaction(
+      transactions,
+      {
+        payload: { chainId, from, hash, hashType, approval, summary, presign, safeTransaction, claim, data }
+      }: PayloadAction<AddTransactionParams>
+    ) {
       if (transactions[chainId]?.[hash]) {
-        throw Error('Attempted to add existing transaction.')
+        console.warn('[state::enhancedTransactions] Attempted to add existing transaction', hash)
+        // Unknown transaction. Do nothing!
+        return
       }
       const txs = transactions[chainId] ?? {}
-      txs[hash] = { hash, approval, summary, claim, from, addedTime: now() }
+      txs[hash] = {
+        hash,
+        transactionHash: hashType === HashType.ETHEREUM_TX ? hash : undefined,
+        hashType,
+        addedTime: now(),
+        from,
+        summary,
+        data,
+
+        // Operations
+        approval,
+        presign,
+        safeTransaction,
+        claim
+      }
       transactions[chainId] = txs
-    })
-    .addCase(clearAllTransactions, (transactions, { payload: { chainId } }) => {
+    },
+    clearAllTransactions(transactions, { payload: chainId }: PayloadAction<number>) {
       if (!transactions[chainId]) return
       transactions[chainId] = {}
-    })
-    .addCase(checkedTransaction, (transactions, { payload: { chainId, hash, blockNumber } }) => {
+    },
+
+    checkedTransaction(
+      transactions,
+      { payload: { chainId, hash, blockNumber } }: PayloadAction<WithChainIdAndHash & { blockNumber: number }>
+    ) {
       const tx = transactions[chainId]?.[hash]
       if (!tx) {
         return
       }
-      if (!tx.lastCheckedBlockNumber) {
-        tx.lastCheckedBlockNumber = blockNumber
-      } else {
-        tx.lastCheckedBlockNumber = Math.max(blockNumber, tx.lastCheckedBlockNumber)
-      }
-    })
-    .addCase(finalizeTransaction, (transactions, { payload: { hash, chainId, receipt } }) => {
+      updateBlockNumber(tx, blockNumber)
+    },
+
+    finalizeTransaction(
+      transactions,
+      {
+        payload: { hash, chainId, receipt }
+      }: PayloadAction<WithChainIdAndHash & { receipt: SerializableTransactionReceipt }>
+    ) {
       const tx = transactions[chainId]?.[hash]
       if (!tx) {
         return
       }
       tx.receipt = receipt
       tx.confirmedTime = now()
-    })
-)
+    },
+
+    replaceTransaction(
+      transactions,
+      {
+        payload: { chainId, oldHash, newHash, type }
+      }: PayloadAction<WithChainId & { oldHash: string; newHash: string; type: ReplacementType }>
+    ) {
+      if (!transactions[chainId]?.[oldHash]) {
+        console.error('Attempted to replace an unknown transaction.')
+        return
+      }
+      const allTxs = transactions[chainId] ?? {}
+      allTxs[newHash] = {
+        ...allTxs[oldHash],
+        hash: newHash,
+        transactionHash: newHash,
+        addedTime: new Date().getTime(),
+        replacementType: type
+      }
+      delete allTxs[oldHash]
+    },
+
+    updateSafeTransaction(
+      transactions,
+      {
+        payload: { chainId, safeTransaction, blockNumber }
+      }: PayloadAction<WithChainId & { safeTransaction: SafeMultisigTransactionResponse; blockNumber: number }>
+    ) {
+      const { safeTxHash, transactionHash } = safeTransaction
+      const tx = transactions[chainId]?.[safeTxHash]
+      if (!tx) {
+        console.warn('[updateSafeTransaction] Unknown safe transaction', safeTxHash)
+        return
+      }
+
+      // Update block number
+      updateBlockNumber(tx, blockNumber)
+
+      // Update tx hash (if present)
+      tx.transactionHash = transactionHash
+
+      // Update safe info
+      tx.safeTransaction = safeTransaction
+    }
+  }
+})
+
+export const {
+  addTransaction,
+  clearAllTransactions,
+  checkedTransaction,
+  finalizeTransaction,
+  replaceTransaction,
+  updateSafeTransaction
+} = transactionsSlice.actions
+export const transactions = transactionsSlice.reducer
